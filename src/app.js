76f82fef1919cc5254c3ae5a506c428da72e67b0
Alberto Araújo <albertoafaj@hotmail.com>
2023-02-03 09:50:12 -0300

    chore: install consign

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 const app = require('express')();
+const consign = require('consign');
+
+consign({ cwd: 'src', verbose: false })
+  .include('./config/middlewares.js')
+  .into(app);
 
 app.get('/', (req, res) => {
   res.status(200).send();
+});
+
+app.get('/user', (req, res) => {
+  const users = [
+    {
+      name: 'Peter Parker',
+      email: 'peter@email.com',
+    },
+  ];
+  res.status(200).json(users);
+});
+
+app.post('/user', (req, res) => {
+  res.status(201).json(req.body);
 });
 
 module.exports = app;
---
+++ src/user.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const app = require('./app');
+
+test('Should to list all users', async () => {
+  const result = await request(app).get('/user');
+  expect(result.status).toBe(200);
+  expect(result.body).toHaveLength(1);
+  expect(result.body[0]).toHaveProperty('name', 'Peter Parker');
+});
+
+test('Should insert a user with sucess', async () => {
+  const result = await request(app)
+    .post('/user')
+    .send({
+      name: 'Steve Rogers',
+      email: 'captain@ranek.com',
+    });
+  expect(result.status).toBe(201);
+  expect(result.body.name).toBe('Steve Rogers');
+});
