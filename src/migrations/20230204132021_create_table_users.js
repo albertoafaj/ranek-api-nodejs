ac9648ec5c8a799648e55d297aecdcee74611dff
Alberto Araújo <albertoafaj@hotmail.com>
2023-02-06 10:50:36 -0300

    refact: user table structure

--- src/migrations/20230204132021_create_table_users.js
+++ src/migrations/20230204132021_create_table_users.js
@@ -4,7 +4,13 @@ exports.up = (knex) => knex.schema.createTable('users', (t) => {
   t.increments('id').primary();
   t.string('name').notNull();
   t.string('email').notNull().unique();
-  t.string('passwd').notNull();
+  t.string('password').notNull();
+  t.string('street');
+  t.string('number');
+  t.string('city');
+  t.string('state');
+  t.string('zipCode');
+  t.string('district');
 });
 
 exports.down = (knex) => {
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,16 +1,12 @@
-module.exports = () => {
-  const findAll = (req, res) => {
-    const users = [
-      {
-        name: 'Peter Parker',
-        email: 'peter@email.com',
-      },
-    ];
+module.exports = (app) => {
+  const findAll = async (req, res) => {
+    const users = await app.db('users').select();
     res.status(200).json(users);
   };
 
-  const create = (req, res) => {
-    res.status(201).json(req.body);
+  const create = async (req, res) => {
+    const result = await app.db('users').insert(req.body, '*');
+    res.status(201).json(result[0]);
   };
 
   return { findAll, create };
